41a0eafda25da81f465ed35a5085300eb1f6c7d2
rocketlaunchr-cto <rocketlaunchr.cloud@gmail.com>
2018-12-19 22:20:42 +1100

    - spelling mistake

--- addressable/addressable.go
+++ addressable/addressable.go
@@ -13,10 +13,10 @@ import (
 
 const randLength = 15 // TODO: Make this configurable
 
-var encountredBlocks blocks
+var encounteredBlocks blocks
 
 func init() {
-	encountredBlocks = blocks{}
+	encounteredBlocks = blocks{}
 }
 
 func Process(tempFile string) error {
@@ -45,15 +45,15 @@ func pre(c *astutil.Cursor) bool {
 
 	switch n := currentNode.(type) {
 	case *ast.BlockStmt:
-		encountredBlocks.add(n)
+		encounteredBlocks.add(n)
 	case *ast.AssignStmt, *ast.BadStmt, *ast.BranchStmt, *ast.DeclStmt, *ast.DeferStmt,
 		*ast.EmptyStmt, *ast.ExprStmt, *ast.ForStmt, *ast.GoStmt, *ast.IfStmt, *ast.IncDecStmt,
 		*ast.LabeledStmt, *ast.RangeStmt, *ast.ReturnStmt, *ast.SelectStmt, *ast.SendStmt,
 		*ast.SwitchStmt, *ast.TypeSwitchStmt:
-		if currentBlock := encountredBlocks.current(); currentBlock != nil {
+		if currentBlock := encounteredBlocks.current(); currentBlock != nil {
 			// Search in current block to see if this stmt is a direct child of parent "blockStmt"
 			if idx, exists := currentBlock.lookup[n.(ast.Stmt)]; exists {
-				encountredBlocks[len(encountredBlocks)-1].current = &[]int{idx}[0]
+				encounteredBlocks[len(encounteredBlocks)-1].current = &[]int{idx}[0]
 			}
 		}
 	case *ast.UnaryExpr:
@@ -63,23 +63,23 @@ func pre(c *astutil.Cursor) bool {
 			case *ast.Ident:
 				// Note: Assume "true" and "false" are not redefined from default boolean type
 				if t.Name == "true" || t.Name == "false" {
-					if currentBlock := encountredBlocks.current(); currentBlock != nil {
+					if currentBlock := encounteredBlocks.current(); currentBlock != nil {
 						n.X = insertSingleLine("bool", t.Name)
 					}
 				}
 			case *ast.CallExpr:
-				if currentBlock := encountredBlocks.current(); currentBlock != nil {
+				if currentBlock := encounteredBlocks.current(); currentBlock != nil {
 					varName := insertCallExpr(currentBlock.node, *currentBlock.current, t)
 					n.X = replaceX(varName)
 				}
 			case *ast.BasicLit:
 				switch t.Kind {
 				case token.STRING:
-					if currentBlock := encountredBlocks.current(); currentBlock != nil {
+					if currentBlock := encounteredBlocks.current(); currentBlock != nil {
 						n.X = insertSingleLine("string", t.Value)
 					}
 				case token.INT, token.FLOAT, token.IMAG, token.CHAR:
-					if currentBlock := encountredBlocks.current(); currentBlock != nil {
+					if currentBlock := encounteredBlocks.current(); currentBlock != nil {
 						varName := insertConstVar(currentBlock.node, t.Kind, *currentBlock.current, t.Value)
 						n.X = replaceX(varName)
 					}
@@ -97,7 +97,7 @@ func post(c *astutil.Cursor) bool {
 
 	switch currentNode.(type) {
 	case *ast.BlockStmt:
-		encountredBlocks.remove()
+		encounteredBlocks.remove()
 	}
 
 	return true
